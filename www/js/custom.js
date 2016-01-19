6d31b89caf7a21ff14f0cf1c1029a1c6b33ce872
Guillaume Beuzelin <Ankanitti@Guillaumes-MacBook-Pro.local>
2016-01-19 11:59:30 +0100

    added flip animation

--- www/js/custom.js
+++ www/js/custom.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
+  
   console.log('jQuery correctly implemented...');
 
+  /* create .back and .front div for jqeury.flip() */
+  var back = $('<div class="back"></div>');
+  var front = $('<div class="front"></div>');
+  $('.carte').append(front);
+  $('.carte').append(back);
+
+  /* enable jquery.flip() */
+  $('.carte').flip();
+
+  /* add card backs */
   console.log('There are '+ $('.carte').length + ' cards on the grid...');
-  $.each($('.carte'),function(){
+  $.each($('.carte .front'),function(){
     var rng = Math.floor((Math.random()*(3)));
     if(rng == 0){
       $(this).addClass('bg-blue');
@@ -12,4 +23,12 @@ $(document).ready(function(){
       $(this).addClass('bg-red');
     }
   });
+
+  /* Color arrays for card fronts */
+  var blue=[];
+  var red=[];
+  var yellow=[];
+
+
+
 });
