448c3bd48e0ad1e4794b48a22e297820e4ce14bc
Ankanitti <b.guillaume.iim@gmail.com>
2016-01-20 23:25:15 +0100

    refacto/score funcs

--- www/js/custom.js
+++ www/js/custom.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
   var randColor = Math.floor(Math.random()*colorPalette.length);
 
   /* apply array to card .front */
-  $.each($('.carte .back'),function(){
+  $.each($('.carte .back'),function(){ /* iterate throw each el */
     var color = colorPalette[randColor];
     var rng = Math.floor((Math.random()*(color.length)));
     $(this).css('background-color',color[rng]);
@@ -44,11 +44,16 @@ $(document).ready(function(){
     trigger : 'manual'
   });
 
+
+
+  /* CORE GAME VARs AND FUNCs */
+
   /* emplty array for matching verif */
-  var picks = [];
-  var timers = [];
-  var count = 0;
-  var penalty = 0;
+  var picks = []; /* array to store rgb code of selected cards */
+  var timers = []; /* array to sotre .now() in order to substract from score */
+  var penalty = 0; /* score to be deducted from final score */
+  var count = 0; /* counter to keep track of matching pairs */
+  var finalScore = 100000; /* initial score */
 
   /* reset card to initial */
   function returnToInit(){
@@ -66,6 +71,11 @@ $(document).ready(function(){
     $('.carte').removeClass('locked'); /* un'locked' class to re-enable pointer-events and flip() */
   };
 
+  /* empty BOTH picks AND timers arrays */
+  function emptyArrays(){
+    picks.splice(0,2);
+    timers.splice(0,2);
+  };
 
   /* flip event handler */
   $('.carte').on('click',function(){
@@ -75,41 +85,31 @@ $(document).ready(function(){
            .toggleClass('locked'); /* lock this card to prevent subsequent re-flipping */
     picks.push($(this).find('.back').css('background-color')); /* save the selected cards' color code for verif into picks array*/
     console.log(picks);
-    if(picks.length==2){ /* condition to init verif */
-      lockAll(); /* if 2 cards are selected lock all the rest */
+    if(picks.length==2){ /* condition to init verification */
+      lockAll(); /* if 2 cards are 'selected' lock all the other cards */
       penalty += (timers[0]-timers[1]); /* add time spent to penalty pool */
-      console.log('Array has 2 values, matching...');
+      console.log('penalty is at : '+penalty);
       if(picks[0]!== picks[1]){ /* IF the color codes don't match... */
         console.log('No match... returning to init...');
-        setTimeout(returnToInit,1100); /* return cards to initial position */
-        picks.splice(0,2); /* empty picks array for following try */
-        setTimeout(unlockAll,1100); /* unlock all locked cards to proceed */
-        timers.splice(0,2);
+        emptyArrays();  /* empty picks and timers array for following try */
+        setTimeout(unlockAll,1000); /* unlock all locked cards to proceed, lowered from 1100ms */
+        setTimeout(returnToInit,1000); /* return cards to initial position, lowered from 1100ms */
       } else { /* IF the color codes match... */
-        count += 1; /* counts the number of matched pairs */
-        timers.splice(0,2); /* empty timers array for next penalty */
+        count += 1; /* tracking the number of matched pairs */
         if(count == 16){
           console.log('All pairs matched... ending game...');
-          finalScore -= penalty;
           console.log('Your final score is : '+finalScore+' !');
+          finalScore += penalty;
         }else{
           console.log('Match!');
           $('.carte.selected').toggleClass('selected') /*remove selected class AND... */
                               .addClass('permaLock'); /* permanently lock them so that they may be flipped no longer */
-          picks.splice(0,2); /* empty picks array for following try */
-          timers.splice(0,2);
+          emptyArrays();
           setTimeout(unlockAll,1100); /* unlock all locked cards to proceed */
           console.log(count);
         };
       };
     };
   });
-
-  /* score calc */
-  /* on flip() start a timer (ms)
-     on second flip() stop the timer and calc diff between two clicks.
-     deduct from max points the time spent 'guessing' in between two clicks */
-
-     var finalScore = 100000;
 
 });
